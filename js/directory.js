512a670c67125a218ba8f1d0a27b8d5aaaabc431
Shaobo Gu <shaobo.gu@appdynamics.com>
2017-04-29 19:33:32 -0700

    fix directory functionality

--- js/directory.js
+++ js/directory.js
@@ -1,15 +1,25 @@
 
-class WIPDirectory {
-    static createPost() {
+//class WIPDirectory {
+//    static createPost() {
+//        var item = new wp.api.models.Directory({
+//            title: "Generated Item " + Date.now(),
+//            content: "Generated Item content: " + Date.now()
+//        });
+//        item.save();
+//        
+//        var items = new wp.api.collections.Directory();
+//        items.fetch().done(function (data) {
+//            var d = data;
+//        });
+//    }
+//}
+
+var WIPDirectory = {
+    createPost: function() {
         var item = new wp.api.models.Directory({
             title: "Generated Item " + Date.now(),
             content: "Generated Item content: " + Date.now()
         });
         item.save();
-        
-//        var items = new wp.api.collections.Directory();
-//        items.fetch().done(function (data) {
-//            var d = data;
-//        });
     }
 }
